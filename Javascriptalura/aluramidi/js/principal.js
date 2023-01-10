457e5c7d7dc2de3e9662b2ecb2456212499bf66a
rodrigoft91 <107896164+rodrigoft91@users.noreply.github.com>
2023-01-10 14:48:33 -0300

    curso continuação

---
+++ Javascriptalura/aluramidi/js/principal.js
@@ -0,0 +1,21 @@
+function tocaSomPom() {
+
+    document.querySelector('#som_tecla_pom').play();
+
+}
+
+
+function tocaSomClap(){
+
+    document.querySelector('#som_tecla_clap').play()
+
+    
+
+}
+
+function tocaSomTim(){
+
+    document.querySelector('#som_tecla_tim').play()
+}
+
+
